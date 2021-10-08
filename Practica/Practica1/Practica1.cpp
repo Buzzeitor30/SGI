db8eeeb29cd60d31beebfaf8a92479e134c70867
Elias Urios Alacreu <thebuzzeitor@gmail.com>
2021-10-08 16:11:57 +0200

    pl01 done

---
+++ Practica/Practica1/Practica1.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <gl\freeglut.h> 
+void display()
+{
+	glClearColor(0, 0, .3, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+void reshape(GLint w, GLint h)
+{
+}
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 400);
+	glutInitWindowPosition(50, 600);
+	glutCreateWindow("Primer programa en OpenGL");
+	std::cout << "OpenGL running " << std::endl;
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+}
